3ad089633b802ffb759b8db9555c6ef22630a84a
Viram Nagar <viramnagar1019@gmail.com>
2025-11-14 21:27:47 +0530

    changing number of colunm

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -35,7 +35,7 @@ ${
         ))}
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6">
         {filteredProducts.map((product) => (
           <ProductCard
             key={product.id}
